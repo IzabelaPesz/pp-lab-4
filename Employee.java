f26a722ec866de78981abe77ef8567f582b792a2
IzabelaPesz <166315384+IzabelaPesz@users.noreply.github.com>
2024-04-07 18:53:26 +0200

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,1 +1,22 @@
+class Employee { 
+  
+      private var fullName: String 
+      private var salary: double
+      
+      public string getName () {
+        return fullName;
+      }
+      public double getSalary () {
+        return salary;
+      }
+        public double setSalary () {
+        salary = 5000;
+          return salary;
+      }
+         public string toString () {
+         return "Employee: " + fullName + ", Salary:" + salary;
+      }
+  
+}
+
 
