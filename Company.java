98fc732ebb129cc2a61214ded253f59c049c3033
IzabelaPesz <166315384+IzabelaPesz@users.noreply.github.com>
2024-04-07 19:35:52 +0200

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,1 +1,15 @@
-
+class Company() {
+public static void main(String[] args) {
+ 		int[][] Employees = new int[5][2];
+  Employees[0][0]="Kowalski"; Employees[0][1]=1000;
+  Employees[1][0]="Nowak"; Employees[1][1]=2000;
+  Employees[2][0]="Schmidt"; Employees[2][1]=3000;  
+  Employees[3][0]="Pesz"; Employees[3][1]=4000;
+  Employees[4][0]="Kwiatkowski"; Employees[4][1]=5000;
+  System.out.println("Dane pracownika: " + Employess[3][0] + Employess[3][1]);
+  Employees[3][1]=5000;
+  for(int i= 0; i< Employees.lenght; i++) {
+    System.out.print(Employees[i][0] + Employees[i][1] );
+  }
+}
+}
